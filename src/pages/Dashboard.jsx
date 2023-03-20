fac1d798ca3e12713c31d35bac508fc1074f69c0
Omar Melendrez <omar.melendrez@gmail.com>
2023-03-20 15:15:10 -0300

    Secure routes

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+export const Dashboard = () => {
+  return (
+    <article>
+      <header>
+        This is the Dashboard page
+      </header>
+      <p>
+        Here is some text
+      </p>
+      <footer>
+        Footer
+      </footer>
+    </article>
+  );
+};
